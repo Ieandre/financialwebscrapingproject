57f07fe05da91c6b6d8f449f54c0bed8ba606331
Ieandre <fernandez.leandre@gmail.com>
2022-01-31 21:18:22 +0100

    Ajout de l'horodatage du fichier résultat

--- webscrappingproject.py
+++ webscrappingproject.py
@@ -1,15 +1,20 @@
+from unicodedata import name
 import urllib.request
 import openpyxl
 from bs4 import BeautifulSoup
 import xlsxwriter
+import time
+
+timestr = time.strftime("%d-%m-%Y_%H-%M-%S")
+
+namefile = str(timestr)+'.xlsx'
+
+
 
 wb = openpyxl.load_workbook(r'test.xlsx')
 sheet = wb.active
 
-workbook = xlsxwriter.Workbook('test.xlsx')
-worksheet = workbook.get_worksheet_by_name('Sheet1')
-#worksheet = workbook.add_worksheet()
-print(worksheet)
+
 
 def Convert(string):
     li = list(string.split(" "))
@@ -34,7 +39,6 @@ for cell in sheet["F"]:
       print(capitalisation)
       for i in range(7):
         if capitalisation[i] != '\n':
-          # worksheet.write(row, ord('H')-ord('A')+1+i, capitalisation[i])
           sheet[chr(ord("N")+i)][row-1].value = capitalisation[i]
 
 
@@ -53,4 +57,5 @@ for cell in sheet["F"]:
           sheet[chr(ord("S")+i)][row-1].value = bna[i]
 
 
-wb.save('test33.xlsx')
+
+wb.save(namefile)
