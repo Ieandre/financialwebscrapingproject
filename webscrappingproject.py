3316a4a458e110f45f11f66e662d6c0c9a2a505a
Ieandre <fernandez.leandre@gmail.com>
2022-01-31 23:13:08 +0100

    Modification du fichier source, et résolution de bugs (pour rentrer les valeurs de BNA)

--- webscrappingproject.py
+++ webscrappingproject.py
@@ -14,7 +14,14 @@ namefile = str(timestr)+'.xlsx'
 wb = openpyxl.load_workbook(r'test.xlsx')
 sheet = wb.active
 
-
+def col(s, n):
+  if len(s) == 1:
+    c = ord(s) + n 
+    if c > ord('Z'):
+      return "A"+chr(c-26)
+    return chr(c)
+  else:
+    return "A"+col(s[1:], n)
 
 def Convert(string):
     li = list(string.split(" "))
@@ -24,7 +31,7 @@ def Convert(string):
 
 
 row = 0
-for cell in sheet["F"]:
+for cell in sheet["C"]:
   row = row+1
   print(row)
   if type(cell.value) is str:
@@ -39,7 +46,7 @@ for cell in sheet["F"]:
       print(capitalisation)
       for i in range(7):
         if capitalisation[i] != '\n':
-          sheet[chr(ord("N")+i)][row-1].value = capitalisation[i]
+          sheet[chr(ord("M")+i)][row-1].value = capitalisation[i]
 
 
       pertemp = Convert(tableau1.findAll('tr')[3].get_text(" "))
@@ -47,14 +54,15 @@ for cell in sheet["F"]:
       print(per)
       for i in range(7):
         if per[i] != '\n':
-          sheet[chr(ord("H")+i)][row-1].value = per[i]
+          sheet[chr(ord("E")+i)][row-1].value = per[i]
 
       bnatemp = Convert(tableau2.findAll('tr')[8].get_text(" "))
       bna = bnatemp[4:11]
       print(bna)
       for i in range(7):
         if bna[i] != '\n':
-          sheet[chr(ord("S")+i)][row-1].value = bna[i]
+          print(i)
+          sheet[col('U', i)][row-1].value = bna[i]
 
 
 
