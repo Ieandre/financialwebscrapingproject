dc103be2859c24071678078013b314a8d87aaada
Ieandre <fernandez.leandre@gmail.com>
2022-01-31 20:18:57 +0100

    Automatisation de la récupération des différentes valeurs pour tout les liens renseignés + ajout d'un fichier de base

--- webscrappingproject.py
+++ webscrappingproject.py
@@ -1,15 +1,27 @@
 import urllib.request
 import openpyxl
 from bs4 import BeautifulSoup
+import xlsxwriter
 
 wb = openpyxl.load_workbook(r'test.xlsx')
 sheet = wb.active
+
+workbook = xlsxwriter.Workbook('test.xlsx')
+worksheet = workbook.get_worksheet_by_name('Sheet1')
+#worksheet = workbook.add_worksheet()
+print(worksheet)
 
 def Convert(string):
     li = list(string.split(" "))
     return li
 
+
+
+
+row = 0
 for cell in sheet["F"]:
+  row = row+1
+  print(row)
   if type(cell.value) is str:
     if cell.value != "Link":
       print(cell.value)
@@ -20,11 +32,25 @@ for cell in sheet["F"]:
       capitalisationtemp = Convert(tableau1.findAll('tr')[5].get_text(" "))
       capitalisation = capitalisationtemp[5:12]
       print(capitalisation)
+      for i in range(7):
+        if capitalisation[i] != '\n':
+          # worksheet.write(row, ord('H')-ord('A')+1+i, capitalisation[i])
+          sheet[chr(ord("N")+i)][row-1].value = capitalisation[i]
+
 
       pertemp = Convert(tableau1.findAll('tr')[3].get_text(" "))
       per = pertemp[3:10]
       print(per)
+      for i in range(7):
+        if per[i] != '\n':
+          sheet[chr(ord("H")+i)][row-1].value = per[i]
 
       bnatemp = Convert(tableau2.findAll('tr')[8].get_text(" "))
       bna = bnatemp[4:11]
       print(bna)
+      for i in range(7):
+        if bna[i] != '\n':
+          sheet[chr(ord("S")+i)][row-1].value = bna[i]
+
+
+wb.save('test33.xlsx')
