8c5b6b66e64ad5cc10a460a35cdf2331daa201c9
Ieandre <fernandez.leandre@gmail.com>
2022-02-06 21:55:57 +0100

    Début d'interface graphique

--- webscrappingproject.py
+++ webscrappingproject.py
@@ -1,10 +1,13 @@
+from tkinter import filedialog
 from unicodedata import name
 import urllib.request
 import openpyxl
 from bs4 import BeautifulSoup
 import xlsxwriter
 import time
+from tkinter import *
 
+"""
 timestr = time.strftime("%d-%m-%Y_%H-%M-%S")
 
 namefile = str(timestr)+'.xlsx'
@@ -32,7 +35,7 @@ def Convert(string):
 
 row = 0
 row2=0
-
+compteurpasvide = 0
 
 for cell in sheet["C"]:
   row = row+1
@@ -57,8 +60,11 @@ for cell in sheet["C"]:
         per = pertemp[3:11]
         print(per)
         for i in range(8):
-          if per[i] != '\n':
-            sheet[chr(ord("E")+i)][row-1].value = per[i]
+          try:
+            if per[i] != '\n':
+              sheet[chr(ord("E")+i)][row-1].value = per[i]
+          except IndexError:
+            print("Problème avec le nombre de données")
 
 
         bnatemp = Convert(tableau2.findAll('tr')[8].get_text(" "))
@@ -94,3 +100,28 @@ for cell in sheet["D"]:
 
 
 wb.save(namefile)
+"""
+#Partie GUI
+
+def openFile():
+  filepath= filedialog.askopenfilename()
+  print(filepath)
+  return filepath
+
+root = Tk()
+root.minsize(550, 400)
+root.title("Financial Web Scraping Project")
+frame = Frame(root, bg="#1e1e1e")
+
+
+Label(frame, text="FINANCIAL WEB SCRAPING", bg="#1e1e1e", font=("Arial Bold", 18,), fg= "white" ).grid(row=0, column=0, ipadx=20, ipady=20)
+button = Button(frame, text="Ouvrir votre fichier Excel", command=openFile)
+button.grid(row=1, column=0)
+
+
+frame.pack()
+frame.place(relx = 0,
+                   rely = 0,
+                   anchor = 'nw')
+root.configure(background='#1e1e1e')
+root.mainloop()
